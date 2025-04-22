fee05b22620b2ca9542a7b7ba1a3d6402ffd33c3
Mauro Joaquin Escudero <maurojoaquinescudero@gmail.com>
2025-04-22 06:45:26 -0300

    feat: allow lsp path config  (#2)

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,19 @@
+import { existsSync } from "fs";
 import * as vscode from "vscode";
 
 import LanguageServerModule from "./modules/languageServer";
 import WebviewModule from "./modules/webview";
 
 export function activate(context: vscode.ExtensionContext) {
-  const lspClient = LanguageServerModule.activate(context);
+  const config = vscode.workspace.getConfiguration('tx3');
+  
+  const lspPath = (config.get('lspPath') as string).replace("~", process.env.HOME!);
+  if (!existsSync(lspPath)) {
+    vscode.window.showErrorMessage(`Tx3 Language Server not found at ${lspPath}`);
+    return;
+  }
+
+  const lspClient = LanguageServerModule.activate(context, lspPath);
   WebviewModule.activate(context, lspClient);
 }
 
