f208bf3218555370dbfa5faa9cb44142922980ac
sofia-bobbiesi <sofiabobbiesi@gmail.com>
2025-05-28 16:52:48 -0300

    refactor: improve code formatting and consistency in DiagramPanel component

--- frontend/src/webviews/DiagramPanel.tsx
+++ frontend/src/webviews/DiagramPanel.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { JSX, useEffect, useState } from "react";
 
 import Title from "../components/Title";
 
 import type { Program, TxDef } from "../types/ast";
 
-type PartyType = 'unknown' | 'party' | 'policy';
+type PartyType = "unknown" | "party" | "policy";
 
 interface Party {
   name: string;
@@ -20,43 +20,43 @@ const getInputParties = (ast: Program, tx: TxDef): Party[] => {
   const parties: Set<string> = new Set();
   for (const input of tx.inputs) {
     for (const field of input.fields) {
-      if ('From' in field) {
-        if ('Identifier' in field.From) {
+      if ("From" in field) {
+        if ("Identifier" in field.From) {
           parties.add(field.From.Identifier.value);
         }
       }
     }
   }
-  return Array.from(parties).map(name => ({
+  return Array.from(parties).map((name) => ({
     name,
-    type: inferPartyType(ast, name)
+    type: inferPartyType(ast, name),
   }));
-}
+};
 
 const getOutputParties = (ast: Program, tx: TxDef): Party[] => {
   const parties: Set<string> = new Set();
   for (const output of tx.outputs) {
     for (const field of output.fields) {
-      if ('To' in field) {
-        if ('Identifier' in field.To) {
+      if ("To" in field) {
+        if ("Identifier" in field.To) {
           parties.add(field.To.Identifier.value);
         }
       }
     }
   }
-  return Array.from(parties).map(name => ({
+  return Array.from(parties).map((name) => ({
     name,
-    type: inferPartyType(ast, name)
+    type: inferPartyType(ast, name),
   }));
-}
+};
 
 const getInputs = (tx: TxDef): Parameter[] => {
   const inputs: Parameter[] = [];
   for (const txInput of tx.inputs) {
-    const input: Parameter = { name: txInput.name};
+    const input: Parameter = { name: txInput.name };
     for (const field of txInput.fields) {
-      if ('From' in field) {
-        if ('Identifier' in field.From) {
+      if ("From" in field) {
+        if ("Identifier" in field.From) {
           input.party = field.From.Identifier.value;
         }
       }
@@ -64,15 +64,15 @@ const getInputs = (tx: TxDef): Parameter[] => {
     inputs.push(input);
   }
   return inputs;
-}
+};
 
 const getOutputs = (tx: TxDef): Parameter[] => {
   const outputs: Parameter[] = [];
   for (const [index, txOutput] of tx.outputs.entries()) {
-    const output: Parameter = { name: txOutput.name || `output ${index+1}` };
+    const output: Parameter = { name: txOutput.name || `output ${index + 1}` };
     for (const field of txOutput.fields) {
-      if ('To' in field) {
-        if ('Identifier' in field.To) {
+      if ("To" in field) {
+        if ("Identifier" in field.To) {
           output.party = field.To.Identifier.value;
         }
       }
@@ -80,46 +80,54 @@ const getOutputs = (tx: TxDef): Parameter[] => {
     outputs.push(output);
   }
   return outputs;
-}
+};
 
 const inferPartyType = (ast: Program, name: string): PartyType => {
   for (const party of ast.parties) {
     if (party.name === name) {
-      return 'party';
+      return "party";
     }
   }
   for (const policy of ast.policies) {
     if (policy.name === name) {
-      return 'policy';
+      return "policy";
     }
   }
-  return 'unknown';
-}
+  return "unknown";
+};
 
 interface PartyProps {
   party: Party;
-  x: number|string;
-  y: number|string;
+  x: number | string;
+  y: number | string;
 }
 
-const getIconUrl = (type: PartyType): string => (
-  new URL(`../images/${type === 'unknown' ? 'party' : type}.svg`, import.meta.url).href
-);
+const getIconUrl = (type: PartyType): string =>
+  new URL(
+    `../images/${type === "unknown" ? "party" : type}.svg`,
+    import.meta.url,
+  ).href;
 
 const Party: React.FC<PartyProps> = (props: PartyProps) => (
   <svg
-    x={props.x} y={props.y}
-    width={UNIT} height={UNIT}
+    x={props.x}
+    y={props.y}
+    width={UNIT}
+    height={UNIT}
     viewBox={`0 0 ${UNIT} ${UNIT}`}
   >
     <image
-      x="25%" y="15%"
-      width="50%" height="60%"
+      x="25%"
+      y="15%"
+      width="50%"
+      height="60%"
       href={getIconUrl(props.party.type)}
     />
     <text
-      x="50%" y="85%" textAnchor="middle"
-      style={{ fontSize: '14%', fontFamily: 'monospace', fill: '#fff' }}
+      x="50%"
+      y="85%"
+      textAnchor="middle"
+      style={{ fontSize: "14%", fontFamily: "monospace", fill: "#fff" }}
     >
       {props.party.name}
     </text>
@@ -128,27 +136,36 @@ const Party: React.FC<PartyProps> = (props: PartyProps) => (
 
 interface TxProps {
   tx: TxDef;
-  x: number|string;
-  y: number|string;
+  x: number | string;
+  y: number | string;
 }
 
 const Tx: React.FC<TxProps> = (props: TxProps) => (
   <g transform={`translate(-${UNIT})`}>
     <svg
-      x={props.x} y={props.y}
-      width={UNIT*2} height={UNIT*4}
-      viewBox={`0 0 ${UNIT} ${UNIT*2}`}
+      x={props.x}
+      y={props.y}
+      width={UNIT * 2}
+      height={UNIT * 4}
+      viewBox={`0 0 ${UNIT} ${UNIT * 2}`}
     >
       <rect
-        width="100%" height="100%" rx={UNIT/10} ry={UNIT/10}
-        fillOpacity="0" stroke="white" strokeWidth="0.25"
-        strokeLinecap="round" strokeLinejoin="round"
+        width="100%"
+        height="100%"
+        rx={UNIT / 10}
+        ry={UNIT / 10}
+        fillOpacity="0"
+        stroke="white"
+        strokeWidth="0.25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <text
-        x="50%" y="50%"
+        x="50%"
+        y="50%"
         textAnchor="middle"
         dominantBaseline="middle"
-        style={{ fontSize: '10%', fontFamily: 'monospace', fill: '#fff' }}
+        style={{ fontSize: "10%", fontFamily: "monospace", fill: "#fff" }}
       >
         {props.tx.name}
       </text>
@@ -158,36 +175,51 @@ const Tx: React.FC<TxProps> = (props: TxProps) => (
 
 interface ParameterProps {
   parameter: Parameter;
-  x: number|string;
-  y: number|string;
+  x: number | string;
+  y: number | string;
 }
 
 const Parameter: React.FC<ParameterProps> = (props: ParameterProps) => (
-  <g transform={`translate(-${UNIT},${UNIT/2})`}>
+  <g transform={`translate(-${UNIT},${UNIT / 2})`}>
     <svg
-      x={props.x} y={props.y}
-      width={UNIT*2} height={UNIT/2}
-      viewBox={`0 0 ${UNIT} ${UNIT/4}`}
+      x={props.x}
+      y={props.y}
+      width={UNIT * 2}
+      height={UNIT / 2}
+      viewBox={`0 0 ${UNIT} ${UNIT / 4}`}
     >
       <text
-        x="50%" y="10%"
+        x="50%"
+        y="10%"
         textAnchor="middle"
         dominantBaseline="hanging"
-        style={{ fontSize: '10%', fontFamily: 'monospace', fill: '#fff' }}
+        style={{ fontSize: "10%", fontFamily: "monospace", fill: "#fff" }}
       >
         {props.parameter.name}
       </text>
       <line
-        x1="20%" y1="90%" x2="80%" y2="90%"
-        stroke="#fff" strokeWidth="0.25"
+        x1="20%"
+        y1="90%"
+        x2="80%"
+        y2="90%"
+        stroke="#fff"
+        strokeWidth="0.25"
       />
       <line
-        x1="70%" y1="80%" x2="80%" y2="90%"
-        stroke="#fff" strokeWidth="0.25"
+        x1="70%"
+        y1="80%"
+        x2="80%"
+        y2="90%"
+        stroke="#fff"
+        strokeWidth="0.25"
       />
       <line
-        x1="70%" y1="100%" x2="80%" y2="90%"
-        stroke="#fff" strokeWidth="0.25"
+        x1="70%"
+        y1="100%"
+        x2="80%"
+        y2="90%"
+        stroke="#fff"
+        strokeWidth="0.25"
       />
     </svg>
   </g>
@@ -207,7 +239,7 @@ const TxDiagram: React.FC<TxDiagramProps> = (props: TxDiagramProps) => {
   const inputConnections: number[][] = [];
   for (const [inputIndex, input] of inputs.entries()) {
     if (input.party) {
-      const partyIndex = inputParties.findIndex(p => p.name === input.party);
+      const partyIndex = inputParties.findIndex((p) => p.name === input.party);
       if (partyIndex !== -1) {
         inputConnections.push([partyIndex, inputIndex]);
       }
@@ -217,7 +249,9 @@ const TxDiagram: React.FC<TxDiagramProps> = (props: TxDiagramProps) => {
   const outputConnections: number[][] = [];
   for (const [outputIndex, output] of outputs.entries()) {
     if (output.party) {
-      const partyIndex = outputParties.findIndex(p => p.name === output.party);
+      const partyIndex = outputParties.findIndex(
+        (p) => p.name === output.party,
+      );
       if (partyIndex !== -1) {
         outputConnections.push([partyIndex, outputIndex]);
       }
@@ -225,63 +259,72 @@ const TxDiagram: React.FC<TxDiagramProps> = (props: TxDiagramProps) => {
   }
 
   return (
-    <svg width="100%" viewBox={`0 0 ${CANVA_WIDTH} ${CANVA_HEIGHT}`} className="my-16">
-      <Tx x={CANVA_WIDTH/2} y="0" tx={props.tx} />
-      {inputParties.map((party, index) =>
+    <svg
+      width="100%"
+      viewBox={`0 0 ${CANVA_WIDTH} ${CANVA_HEIGHT}`}
+      className="my-16"
+    >
+      <Tx x={CANVA_WIDTH / 2} y="0" tx={props.tx} />
+      {inputParties.map((party, index) => (
+        <Party x="0" y={UNIT * index} party={party} key={index} />
+      ))}
+      {outputParties.map((party, index) => (
         <Party
-          x="0"
-          y={UNIT*index}
+          x={CANVA_WIDTH - UNIT}
+          y={UNIT * index}
           party={party}
           key={index}
         />
-      )}
-      {outputParties.map((party, index) =>
-        <Party
-          x={CANVA_WIDTH-UNIT}
-          y={UNIT*index}
-          party={party}
-          key={index}
-        />
-      )}
-      <g transform={`translate(${UNIT/2})`}>
-        {inputs.map((input, index) =>
+      ))}
+      <g transform={`translate(${UNIT / 2})`}>
+        {inputs.map((input, index) => (
           <Parameter
-            x={CANVA_WIDTH/4}
-            y={UNIT*index}
+            x={CANVA_WIDTH / 4}
+            y={UNIT * index}
             parameter={input}
             key={index}
           />
-        )}
+        ))}
       </g>
-      <g transform={`translate(-${UNIT/2})`}>
-        {outputs.map((output, index) =>
+      <g transform={`translate(-${UNIT / 2})`}>
+        {outputs.map((output, index) => (
           <Parameter
-            x={(CANVA_WIDTH/2)+(CANVA_WIDTH/4)}
-            y={UNIT*index}
+            x={CANVA_WIDTH / 2 + CANVA_WIDTH / 4}
+            y={UNIT * index}
             parameter={output}
             key={index}
           />
-        )}
+        ))}
       </g>
-      {inputConnections.map(([partyIndex, inputIndex], index) =>
+      {inputConnections.map(([partyIndex, inputIndex], index) => (
         <line
           key={index}
-          x1={UNIT} y1={(UNIT*partyIndex)+(UNIT/2)}
-          x2={(CANVA_WIDTH/4)-(UNIT/8)} y2={UNIT*(inputIndex+1)-(UNIT/16)}
-          stroke="#fff" strokeWidth="0.4" strokeDasharray="1,1" strokeOpacity="0.5"
+          x1={UNIT}
+          y1={UNIT * partyIndex + UNIT / 2}
+          x2={CANVA_WIDTH / 4 - UNIT / 8}
+          y2={UNIT * (inputIndex + 1) - UNIT / 16}
+          stroke="#fff"
+          strokeWidth="0.4"
+          strokeDasharray="1,1"
+          strokeOpacity="0.5"
         />
-      )}
-      {outputConnections.map(([partyIndex, outputIndex], index) =>
+      ))}
+      {outputConnections.map(([partyIndex, outputIndex], index) => (
         <line
           key={index}
-          x1={(CANVA_WIDTH/2)+(CANVA_WIDTH/4)+(UNIT/8)} y1={UNIT*(outputIndex+1)-(UNIT/16)}
-          x2={CANVA_WIDTH-UNIT} y2={(UNIT*partyIndex)+(UNIT/2)}
-          stroke="#fff" strokeWidth="0.4" strokeDasharray="1,1" strokeOpacity="0.5"
+          x1={CANVA_WIDTH / 2 + CANVA_WIDTH / 4 + UNIT / 8}
+          y1={UNIT * (outputIndex + 1) - UNIT / 16}
+          x2={CANVA_WIDTH - UNIT}
+          y2={UNIT * partyIndex + UNIT / 2}
+          stroke="#fff"
+          strokeWidth="0.4"
+          strokeDasharray="1,1"
+          strokeOpacity="0.5"
         />
-      )}
+      ))}
     </svg>
   );
-}
+};
 
 const UNIT = 16;
 const CANVA_WIDTH = UNIT * 10;
@@ -289,33 +332,34 @@ const CANVA_HEIGHT = UNIT * 4;
 
 const DiagramPanel: React.FC = () => {
   const [txs, setTxs] = useState<Tx[]>([]);
-  const [ast, setAst] = useState<Program|null>(null);
+  type DiagramItem = { tx_name: string; svg: string };
+  const [diagram, setDiagram] = useState<DiagramItem[] | null>(null);
 
   useEffect(() => {
-    window.addEventListener('message', handleMessage);
-    vscode.postMessage({ event: 'init' });
-    return () => window.removeEventListener('message', handleMessage);
+    window.addEventListener("message", handleMessage);
+    vscode.postMessage({ event: "init" });
+    return () => window.removeEventListener("message", handleMessage);
   }, []);
 
   const handleMessage = (event: MessageEvent<AppEvent>) => {
-    if (event.data.type === 'document-data') {
+    if (event.data.type === "document-data") {
       setTxs(event.data.data.txs);
-      setAst(event.data.data.ast);
-      console.log(event.data.data.ast as Program);
+      setDiagram(event.data.data.diagram);
+      console.log(event.data.data.diagram);
     }
   };
 
   return (
     <div className="root">
       <h3 className="panel-title">Tx3 Diagram</h3>
-      {ast !== null && ast.txs.map((tx: TxDef, index: number) =>
-        <div key={index}>
-          <Title>Tx {tx.name}</Title>
-          <TxDiagram ast={ast} tx={tx} />
+      {diagram?.map(({ tx_name, svg }) => (
+        <div key={tx_name}>
+          <Title>Tx {tx_name}</Title>
+          <div dangerouslySetInnerHTML={{ __html: svg }} />
         </div>
-      )}
+      ))}
     </div>
   );
-}
+};
 
 export default DiagramPanel;
